a28ae0b91a58d67eceb012b1af75fbcdc92333f8
weinstein87 <50801659+matt-weinstein@users.noreply.github.com>
2018-01-12 20:43:30 +0000

    Adding dummy cadaCheckForDerivs routine to return false for any non-overloaded variables.

---
+++ lib/cadaUtils/cadaCheckForDerivs.m
@@ -0,0 +1,9 @@
+function flag = cadaCheckForDerivs(x)
+% Dummy routine for non-overloaded variables, just returns false
+%
+% Copyright 2011-2014 Matthew J. Weinstein and Anil V. Rao
+% Distributed under the GNU General Public License version 3.0
+
+flag = false;
+
+end
